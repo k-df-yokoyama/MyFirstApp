791573496024843d565ded8b730e7524eb03fc33
DESKTOP-A3PUA1E\kensuke <pxd04463@nifty.ne.jp>
2022-03-05 16:49:08 +0900

    * Use View Binding

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -5,17 +5,20 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.example.myfirstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     var count = 0
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val text1 = findViewById<TextView>(R.id.text1)
+        val text1 = binding.text1
         text1.text = "This is sample."
-        val button1 = findViewById<Button>(R.id.button1)
+        val button1 = binding.button1
         button1.setOnClickListener {
             text1.text = "count: " + ++count
             Toast.makeText(this, "tapped!", Toast.LENGTH_SHORT).show()
