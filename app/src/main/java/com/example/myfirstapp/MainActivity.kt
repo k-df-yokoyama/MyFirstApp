622d3e2c3511b87e6cb4f3070b8208dbb835f490
DESKTOP-A3PUA1E\kensuke <pxd04463@nifty.ne.jp>
2022-03-05 17:04:29 +0900

    * Use android extensions

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,23 +2,17 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
-import com.example.myfirstapp.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     var count = 0
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        val text1 = binding.text1
         text1.text = "This is sample."
-        val button1 = binding.button1
         button1.setOnClickListener {
             text1.text = "count: " + ++count
             Toast.makeText(this, "tapped!", Toast.LENGTH_SHORT).show()
