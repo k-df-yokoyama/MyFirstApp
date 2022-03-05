0ba4284fbdb8555da8d6edf7864d815e4a793063
DESKTOP-A3PUA1E\kensuke <pxd04463@nifty.ne.jp>
2022-03-05 16:28:34 +0900

    * Add button

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text1 = findViewById<TextView>(R.id.text1)
+        text1.text = "This is sample."
+        val button1 = findViewById<Button>(R.id.button1)
+        button1.setOnClickListener {
+            text1.text = "count: " + ++count
+            Toast.makeText(this, "tapped!", Toast.LENGTH_SHORT).show()
+        }
     }
+
 }
